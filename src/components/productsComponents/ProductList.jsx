f5952462a4abd8283063d32d8f636b398361e1af
Martin Alan Rodriguez Fisher <marodriguez.dwfc6@ucamp.utel.edu.mx>
2022-11-06 16:56:50 -0600

    Retocando PageProducts y ProfilePage

--- src/components/productsComponents/ProductList.jsx
+++ src/components/productsComponents/ProductList.jsx
@@ -41,7 +41,7 @@ const ProductList = () => {
 
   const displayProducts = products.slice(pagesVisited, pagesVisited + productPerPage).map((product) => {
     return  (
-      <div className="col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-2" key={product.id} >
+      <div className="col-sm-12 col-md-8 col-lg-4 col-xl-4 mb-2" key={product.id} >
         <div className="card text-black"  >
           <img id="img_cardProduct" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Imagen_no_disponible.svg/240px-Imagen_no_disponible.svg.png" className="card-img-top" alt="Producto" />
           <div className="card-body">
--- src/pages/users/ProfilePage.jsx
+++ src/pages/users/ProfilePage.jsx
@@ -10,7 +10,7 @@ const ProfilePage = () => {
     <>
       <Title titulo="Perfil" />
 
-      <div className="col-lg-10 col-md-8 col-sm-12 ms-auto me-auto mb-3">
+      <div className="col-lg-10 col-md-10 col-sm-10 ms-auto me-auto mb-3">
         <div className="card">
           <div className="card-body">
             <div className="d-flex flex-column align-items-center text-center">
@@ -18,7 +18,6 @@ const ProfilePage = () => {
               <div className="mt-3">
                 <h4>{auth.name}  {auth.lastName}</h4>
                 <p className="text-secondary mb-1">{auth.userName}</p>
-                <p className="text-secondary mb-1">{auth.rol}</p>
                 <p className="text-muted font-size-sm">{auth.email}</p>
               </div>
             </div>
